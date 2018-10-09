26f45803cd6ba0fbaae4e0589c7e6c3b275057c8
Elie Bizimana <ebizimana@liberty.edu>
2018-10-08 23:50:30 -0400

    Finializing version 1 of the app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var camps = [
   {name: "Acorn Oaks", image: "https://www.acornoakscampground.net/wp-content/uploads/wppa/53.jpg?ver=1"},
   {name: "Beaver Point", image: "https://api.campgroundbooking.com/api/v1/property/5a39f83dd9c7ef48b9a3b98b/image/Beaver-Point-Resort---Cottages-3e33c50c5056a36_3e33c702-5056-a36a-0a20ccc01a08f6ff_600_thumb.jpg"},
   {name: "Bixler Lake", image: "https://media-cdn.tripadvisor.com/media/photo-s/0a/f3/0c/ca/changing-areas.jpg"}
 ]
+
+app.use(bodyParser.urlencoded({extended:true}))
 app.set("view engine","ejs")
 
 app.get("/", function(req,res){
@@ -12,6 +15,16 @@ app.get("/", function(req,res){
 })
 app.get("/campgrounds",function(req,res){
   res.render("campgrounds",{camps:camps})
+})
+app.post("/campgrounds", function(req,res){
+  var name = req.body.name
+  var image = req.body.image
+  var newCamp = {name: name, image: image}
+  camps.push(newCamp)
+  res.redirect("/campgrounds")
+})
+app.get("/campgrounds/new",function(req,res){
+  res.render("new")
 })
 
 app.listen(3000, function(){
---
+++ readMe.md
@@ -0,0 +1,25 @@
+# YelpCamp Website
+YelpCamp is going to be built in different version that have different objectives
+
+## Version 1
+
+### Initial Setup
+* Add Home Page
+* Add Campgrounds Page that lists all Campgrounds
+  * Each Campground has:
+      * Name
+      * Image
+
+### Layout and Basic Styling
+* Create our header and footer partials
+* Add in BootStrap.
+
+### Creating New Campgorunds
+* Setup new campground Post routes
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+### Style the campgorunds Page
+* Add a better header/title
+* Make campgrounds display in a grid
