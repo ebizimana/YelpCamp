5b84109cd5e58b192daf1a896973a29945384368
Elie Bizimana <ebizimana@liberty.edu>
2018-10-28 12:31:58 -0400

    Version 1 completed and starting on Version 2

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var app = express();
 var camps = [
   {name: "Acorn Oaks", image: "https://www.acornoakscampground.net/wp-content/uploads/wppa/53.jpg?ver=1"},
   {name: "Beaver Point", image: "https://api.campgroundbooking.com/api/v1/property/5a39f83dd9c7ef48b9a3b98b/image/Beaver-Point-Resort---Cottages-3e33c50c5056a36_3e33c702-5056-a36a-0a20ccc01a08f6ff_600_thumb.jpg"},
-  {name: "Bixler Lake", image: "https://media-cdn.tripadvisor.com/media/photo-s/0a/f3/0c/ca/changing-areas.jpg"}
+  {name: "Bixler Lake", image: "https://media-cdn.tripadvisor.com/media/photo-s/0a/f3/0c/ca/changing-areas.jpg"},
+  {name: "Acorn Oaks", image: "https://www.acornoakscampground.net/wp-content/uploads/wppa/53.jpg?ver=1"},
+  {name: "Beaver Point", image: "https://api.campgroundbooking.com/api/v1/property/5a39f83dd9c7ef48b9a3b98b/image/Beaver-Point-Resort---Cottages-3e33c50c5056a36_3e33c702-5056-a36a-0a20ccc01a08f6ff_600_thumb.jpg"},
+  {name: "Acorn Oaks", image: "https://www.acornoakscampground.net/wp-content/uploads/wppa/53.jpg?ver=1"},
+  {name: "Beaver Point", image: "https://api.campgroundbooking.com/api/v1/property/5a39f83dd9c7ef48b9a3b98b/image/Beaver-Point-Resort---Cottages-3e33c50c5056a36_3e33c702-5056-a36a-0a20ccc01a08f6ff_600_thumb.jpg"},
+  {name: "Acorn Oaks", image: "https://www.acornoakscampground.net/wp-content/uploads/wppa/53.jpg?ver=1"}
 ]
 
 app.use(bodyParser.urlencoded({extended:true}))
--- readMe.md
+++ readMe.md
@@ -23,3 +23,13 @@ YelpCamp is going to be built in different version that have different objective
 ### Style the campgorunds Page
 * Add a better header/title
 * Make campgrounds display in a grid
+
+### Style the Navbar and Form
+* Add a navbar to all templates
+* Style the new campground form
+
+## Version 2
+### Add Mongoose
+* Install and configure mongoose
+* Setup campground model
+* Use campground model inside of our routes!
